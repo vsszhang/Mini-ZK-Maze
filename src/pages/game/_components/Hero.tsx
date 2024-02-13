02b4108fe54f9b2f449a0efa19204e05a16b9499
vsszhang <vsszhang@gmail.com>
2024-02-13 22:52:25 +0800

    feat: add game start logic with `Hero` page

--- src/pages/game/_components/Hero.tsx
+++ src/pages/game/_components/Hero.tsx
@@ -1,4 +1,13 @@
+import { useDispatchStore } from "@/store";
+
 export const Hero = () => {
+  const dispatch = useDispatchStore();
+
+  const startClickHandler = () => {
+    console.log("click start");
+    dispatch && dispatch({ type: "start", param: true });
+  };
+
   return (
     <div
       className="min-h-screen hero"
@@ -9,13 +18,13 @@ export const Hero = () => {
       <div className="bg-opacity-60 hero-overlay"></div>
       <div className="text-center text-neutral-content hero-content">
         <div className="max-w-md">
-          <h1 className="font-bold mb-5 text-5xl">ZK Maze</h1>
+          <h1 className="font-bold mb-5 text-5xl">Mini ZK Maze</h1>
           <p className="mb-5">
             In this maze game, wit and strategy are your tools. Find the
             shortest path, skillfully avoid obstacles, and victory is within
             reach. Ready? Adventure is calling!
           </p>
-          <button className="btn btn-primary" onClick={() => {}}>
+          <button className="btn btn-primary" onClick={startClickHandler}>
             Start the game
           </button>
         </div>
