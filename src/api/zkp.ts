0b8edc0494b0808696499e88b9b9027153517426
vsszhang <vsszhang@gmail.com>
2024-02-13 20:41:13 +0800

    refactor: rm unused API

--- src/api/zkp.ts
+++ src/api/zkp.ts
@@ -1,22 +1,4 @@
-import instance, { CHAIN_SERVICE } from "./axiosInstance";
-
-export const upload = (params: FormData) => {
-  return instance.post(`/api/s3/upload`, params, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-  });
-};
-
-export const getMap = () => {
-  return instance.get(`/api/zkmaze/generate`);
-};
-
-export const getETH = (params: { ethAddress: `0x${string}` }) => {
-  return instance.post(`/sendEth`, params, {
-    baseURL: CHAIN_SERVICE,
-  });
-};
+import instance from "./axiosInstance";
 
 export const zkpVerifyLocally = (params: string) => {
   return instance.post(`/api/verify`, params, {
